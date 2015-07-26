b6851764f1bb4f19790d525c60bf937ddac18fbc
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-07-26 02:47:08 -0700

    update adapter to read in store data model

--- app/src/main/java/me/anhvannguyen/android/mapsample/PopupAdapter.java
+++ app/src/main/java/me/anhvannguyen/android/mapsample/PopupAdapter.java
@@ -1,21 +1,36 @@
 package me.anhvannguyen.android.mapsample;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
+import me.anhvannguyen.android.mapsample.model.Store;
 
 /**
  * Created by anhvannguyen on 7/25/15.
  */
 public class PopupAdapter implements GoogleMap.InfoWindowAdapter {
     private View mPopup = null;
+    private Context mContext = null;
     private LayoutInflater mInflater = null;
+    private HashMap<String, Store> mStore = null;
 
-    PopupAdapter(LayoutInflater inflater) {
+    public PopupAdapter(LayoutInflater inflater) {
         mInflater = inflater;
+    }
+
+    public PopupAdapter(Context context, LayoutInflater inflater, HashMap<String, Store> store) {
+        mContext = context;
+        mInflater = inflater;
+        mStore = store;
     }
 
     @Override
@@ -29,11 +44,19 @@ public class PopupAdapter implements GoogleMap.InfoWindowAdapter {
             mPopup = mInflater.inflate(R.layout.marker_popup, null);
         }
 
+        ImageView popupImage = (ImageView) mPopup.findViewById(R.id.popup_imageview);
+        Picasso.with(mContext)
+                .load(mStore.get(marker.getId()).getImagePath())
+                .placeholder(R.mipmap.ic_launcher)
+                .resize(100, 100)
+                .noFade()
+                .into(popupImage);
+
         TextView titleTextView = (TextView) mPopup.findViewById(R.id.popup_title);
-        titleTextView.setText(marker.getTitle());
+        titleTextView.setText(mStore.get(marker.getId()).getTitle());
 
         TextView snippetTextView = (TextView) mPopup.findViewById(R.id.popup_snippet);
-        snippetTextView.setText(marker.getSnippet());
+        snippetTextView.setText(mStore.get(marker.getId()).getSnippet());
 
         return mPopup;
     }
