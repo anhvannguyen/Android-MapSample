18fd0e3317620f2f91cd751dd189cf0db2d1945c
Anh Nguyen <nguyen.anhvan@gmail.com>
2015-07-26 13:13:06 -0700

    fix issue where image does not show on infowindow on first selection

--- app/src/main/java/me/anhvannguyen/android/mapsample/PopupAdapter.java
+++ app/src/main/java/me/anhvannguyen/android/mapsample/PopupAdapter.java
@@ -1,6 +1,7 @@
 package me.anhvannguyen.android.mapsample;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
@@ -22,6 +24,8 @@ public class PopupAdapter implements GoogleMap.InfoWindowAdapter {
     private Context mContext = null;
     private LayoutInflater mInflater = null;
     private HashMap<String, Store> mStore = null;
+    private Marker mLastMarker = null;
+
 
     public PopupAdapter(LayoutInflater inflater) {
         mInflater = inflater;
@@ -44,13 +48,17 @@ public class PopupAdapter implements GoogleMap.InfoWindowAdapter {
             mPopup = mInflater.inflate(R.layout.marker_popup, null);
         }
 
+        if (mLastMarker == null || !mLastMarker.getId().equals(marker.getId())) {
+            mLastMarker = marker;
+        }
+
         ImageView popupImage = (ImageView) mPopup.findViewById(R.id.popup_imageview);
         Picasso.with(mContext)
                 .load(mStore.get(marker.getId()).getImagePath())
                 .placeholder(R.mipmap.ic_launcher)
                 .resize(100, 100)
                 .noFade()
-                .into(popupImage);
+                .into(popupImage, new MarkerCallback(marker));
 
         TextView titleTextView = (TextView) mPopup.findViewById(R.id.popup_title);
         titleTextView.setText(mStore.get(marker.getId()).getTitle());
@@ -59,5 +67,27 @@ public class PopupAdapter implements GoogleMap.InfoWindowAdapter {
         snippetTextView.setText(mStore.get(marker.getId()).getSnippet());
 
         return mPopup;
+    }
+
+    protected static class MarkerCallback implements Callback {
+        private final String LOG_TAG = MarkerCallback.class.getSimpleName();
+        Marker mMarker = null;
+
+        protected MarkerCallback(Marker marker) {
+            mMarker = marker;
+        }
+
+        @Override
+        public void onSuccess() {
+            if (mMarker != null && mMarker.isInfoWindowShown()) {
+                mMarker.hideInfoWindow();
+                mMarker.showInfoWindow();
+            }
+        }
+
+        @Override
+        public void onError() {
+            Log.e(LOG_TAG, "Error loading image");
+        }
     }
 }
